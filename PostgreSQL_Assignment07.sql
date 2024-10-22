bbdde3a48a5cee600dec46a6b61b4a9867ec141a
NafizUddin <nafizuddin.ctg@gmail.com>
2024-10-22 19:36:01 +0600

    completed three queries

--- PostgreSQL_Assignment07.sql
+++ PostgreSQL_Assignment07.sql
@@ -52,3 +52,49 @@ VALUES
 (1, 2),
 (2, 1),
 (3, 2);
+
+
+-- Query-01
+-- Insert a new student record
+INSERT INTO students (student_name, age, email, frontend_mark, backend_mark, status)
+VALUES
+('Niaz', 24, 'niaz@gmail.com', 58, 55, NULL);
+
+-- In the above query,I have added a new student record using insert statement.
+
+-- Query-02
+-- Retrieve the names of all students who are enrolled in the course titled 'Next.js'.
+
+SELECT s.student_name from students s
+JOIN enrollment e ON s.student_id = e.student_id
+JOIN courses c ON e.course_id = c.course_id
+WHERE c.course_name = 'Next.js';
+
+-- In the above query, I have joined the students, enrollment, and courses tables to retrieve the names of the students enrolled in the "Next.js" course. It matches student IDs and course IDs to filter for those specifically enrolled in that course.
+
+-- Query-03
+-- Update the status of the student with the highest total (frontend_mark + backend_mark) to 'Awarded'.
+
+UPDATE students
+SET status = 'Awarded'
+WHERE student_id = (
+    SELECT student_id
+    FROM students
+    ORDER BY (frontend_mark + backend_mark) DESC
+    LIMIT 1
+);
+
+-- In the above query, I have used subquery to identify the student with the highest total and set status to 'Awarded'.
+
+
+-- Query-04
+
+-- Query-05
+
+-- Query-06
+
+-- Query-07
+
+-- Query-08
+
+-- Query-09
